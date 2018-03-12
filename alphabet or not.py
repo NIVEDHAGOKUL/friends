579c8b9e4dba1af1b6beba35f7cd95c96f745f84
NIVEDHAGOKUL <36990399+NIVEDHAGOKUL@users.noreply.github.com>
2018-03-12 17:41:59 +0530

    Update alphabet or not.py

--- alphabet or not.py
+++ alphabet or not.py
@@ -4,6 +4,6 @@ if ch == '0':
     exit();
 else:
     if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    	print(ch, "is an alphabet.");
+    	print(ch, " an alphabet.");
     else:
-    	print(ch, "is not an alphabet.");
+    	print(ch, " not an alphabet.");
